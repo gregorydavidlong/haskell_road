4b9d1b25f36cfdd686e643e30d41a1d6be947782
Gregory Long <gregorydavidlong@gmail.com>
2012-05-02 12:18:39 +1000

    Up to 1.21

--- chapter_1/Lengths.hs
+++ chapter_1/Lengths.hs
@@ -1,4 +1,7 @@
 module Lengths where
 
 lengths :: [[a]] -> [Int]
-lengths xs = (map length xs) 
+lengths xs = (map length xs)
+
+sumlengths :: [[a]] -> Int
+sumlengths xs = sum (map length xs)
--- chapter_1/LengthsSpec.hs
+++ chapter_1/LengthsSpec.hs
@@ -28,3 +28,20 @@ main = hspecX $ do
       it "two lists" $
          lengths [[1,3,4], [2,3,3,3,2,2,2]] `shouldBe` [3, 7] 
 
+  describe "sumlengths" $ do
+    it "works for empty list" $
+      sumlengths [] `shouldBe` 0
+
+    describe "works for single list" $ do
+      it "that is empty" $
+        sumlengths [[]] `shouldBe` 0
+
+      it "that is of length one" $
+        sumlengths [['a']] `shouldBe` 1
+
+      it "that is of greater than length one" $
+        sumlengths [[1,2,3]] `shouldBe` 3
+
+    describe "works for multiple lists" $ do
+      it "two lists" $
+        sumlengths [[1,2,3], [3,2,4,4,2,3]] `shouldBe` 9
