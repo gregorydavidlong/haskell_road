1340c29f9bdde1bf6681dd0fab2b778e5b637223
Gregory Long <gregorydavidlong@gmail.com>
2012-05-02 10:23:36 +1000

    Finished BlowUp

--- chapter_1/BlowUp.hs
+++ chapter_1/BlowUp.hs
@@ -1,4 +1,13 @@
 module BlowUp where
 
 blowup :: String -> String
-blowup "" = ""
+blowup x = blowup_alt x 1
+
+-- source, depth -> result
+blowup_alt :: String -> Int -> String
+blowup_alt "" _ = ""
+blowup_alt (h:t) x = (repeat_string h x) ++ (blowup_alt t (x+1))
+
+repeat_string :: Char -> Int -> String
+repeat_string string 1 = [string]
+repeat_string string times = [string] ++ (repeat_string string (times - 1))
--- chapter_1/BlowUpSpec.hs
+++ chapter_1/BlowUpSpec.hs
@@ -1,20 +1,32 @@
 module BlowUpSpec where
 
 import Test.Hspec.Monadic
+import Test.Hspec.HUnit ()
+import Test.Hspec.QuickCheck
+import Test.HUnit
 import BlowUp
+
+shouldBe :: (Show a, Eq a) => a -> a -> Assertion
+actual `shouldBe` expected = actual @?= expected
 
 main = hspecX $ do
   describe "BlowUp" $ do
 
     it "blowup the empty string" $
-      blowup "" == ""
+      blowup "" `shouldBe` ""
 
     it "blowup single element string" $
-      blowup "a" == "a"
+      blowup "a" `shouldBe` "a"
 
     it "blowup multiple element string" $
-      blowup "abc" == "abbccc"
+      blowup "abc" `shouldBe` "abbccc"
 
     it "blowup the example string" $
-      blowup "bang!" == "baannngggg!!!!!"
+      blowup "bang!" `shouldBe` "baannngggg!!!!!"
+
+
+  describe "repeat_string" $ do
+
+    it "should repeat a single character" $
+      repeat_string 'a' 1 `shouldBe` "a"
 
